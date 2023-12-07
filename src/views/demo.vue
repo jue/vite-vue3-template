5846106a17a51509d532d8f9798a4cad98a05f0d
xiangjianfeng <i@xiangjianfeng.com>
2023-12-07 12:05:54 +0800

    first commit

---
+++ src/views/demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="max-w-[800px] mx-auto bg-yellow-100 h-full">
+    demo
+  </div>
+</template>
+<route>
+  {
+    path: '/login',
+    meta: {
+      title: '用户登陆',
+      layout: 'auth', //布局模板名称
+    }
+  }
+</route>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    vue(),
+    Pages({
+      dirs: 'src/views',
+      exclude: ['**/components/*.vue'],
+      importMode: 'async',
+    }),
+    Layouts({
+      layoutsDirs: 'src/layout', // 布局文件存放目录
+      defaultLayout: 'default', // 默认布局，对应 src/layout/default.vue
+    }),
+    AutoImport({
+      dts: true,
+      dirs: ['./src/utils'],
+      imports: ['vue', 'vue-router'],
+    }),
+    Components({
+      resolvers: [],
+      directoryAsNamespace: true,
+    }),
+  ],
+})
